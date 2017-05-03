27d1b675613af24aaea551658adc6d8f1d511c00
droidbox <info@droidbox.co.uk>
2017-05-03 15:18:41 +0300

    projects/DroidBOX: update filesystem

--- projects/DroidBOX/filesystem/usr/share/bootloader/update.sh
+++ projects/DroidBOX/filesystem/usr/share/bootloader/update.sh
@@ -20,58 +20,28 @@
 
 [ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
 [ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
-[ -z "$UPDATE_DIR" ] && UPDATE_DIR="/storage/.update"
-UPDATE_DTB_IMG="$UPDATE_DIR/dtb.img"
-UPDATE_DTB=`ls -1 "$UPDATE_DIR"/*.dtb 2>/dev/null | head -n 1`
 
-# Indicate that we are not modifying bootloader
-echo "Bootloader has NOT been modified."
-echo "Updating device tree and partition labels..."
+IMAGE_KERNEL="/flash/kernel.img"
 
+# parse command line arguments
 for arg in $(cat /proc/cmdline); do
   case $arg in
-    boot=*)
-      boot="${arg#*=}"
-      case $boot in
-        /dev/mmc*)
-          LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fatlabel $boot "LIBREELEC"
-          ;;
-        LABEL=*)
-          LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fatlabel $($SYSTEM_ROOT/usr/sbin/findfs $boot) "LIBREELEC"
-          ;;
-      esac
-
-      if [ -f "$UPDATE_DTB_IMG" ] ; then
-        UPDATE_DTB_SOURCE="$UPDATE_DTB_IMG"
-      elif [ -f "$UPDATE_DTB" ] ; then
-        UPDATE_DTB_SOURCE="$UPDATE_DTB"
-      fi
-
-      if [ -f "$UPDATE_DTB_SOURCE" ] ; then
-        echo "Updating device tree from $UPDATE_DTB_SOURCE..."
-        case $boot in
-          /dev/system)
-            LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fw_setenv upgrade_step 3 2>/dev/null
-            dd if=$UPDATE_DTB_SOURCE of="/dev/dtb" bs=256k
-            LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fw_setenv upgrade_step 2 2>/dev/null
-            ;;
-          /dev/mmc*|LABEL=*)
-            mount -o rw,remount $BOOT_ROOT
-            cp -f $UPDATE_DTB_SOURCE "$BOOT_ROOT/dtb.img"
-            ;;
-        esac
-      fi
-      ;;
-    disk=*)
-      disk="${arg#*=}"
-      case $disk in
-        /dev/mmc*)
-          LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/e2label $disk "LIBREELEC_DISK"
-          ;;
-        LABEL=*)
-          LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/e2label $($SYSTEM_ROOT/usr/sbin/findfs $disk) "LIBREELEC_DISK"
-          ;;
-      esac
-      ;;
+    BOOT_IMAGE=*)
+      IMAGE_KERNEL="${arg#*=}"
+    ;;
   esac
 done
+
+
+# mount $BOOT_ROOT r/w
+  mount -o remount,rw $BOOT_ROOT
+
+if [ -b $IMAGE_KERNEL ]; then
+  dd if="$SYSTEM_ROOT/usr/share/bootloader/dtb.img" of="/dev/dtb" bs=262144 2>&1
+else
+  cp -p $SYSTEM_ROOT/usr/share/bootloader/dtb.img $BOOT_ROOT
+fi
+
+# mount $BOOT_ROOT r/o
+  sync
+  mount -o remount,ro $BOOT_ROOT
